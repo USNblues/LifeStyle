eb834e0357a7ca90560f87bce5e31b2f5d51ca95
Jorge Garcia <jorgejess@me.com>
2022-05-30 00:26:50 +0900

    Initial Commit

---
+++ Shared/LifeStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeStyleApp.swift
+//  Shared
+//
+//  Created by Jorge Garcia on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LifeStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
